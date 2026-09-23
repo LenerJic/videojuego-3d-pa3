acc65a79e0c585b27b1b4f0c5a6f943ae9e46dcc
Fabiana Monge <60934080@continental.edu.pe>
2026-09-22 20:33:16 -0500

    Menú principal, escena de victoria, musica y efectos de sonido

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
